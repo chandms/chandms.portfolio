69a4974febe333a02d8940764bd6a1c84d574a0a
Chandrika <chandrika.mjee@gmail.com>
2023-04-06 20:07:17 -0400

    Updates

---
+++ precache-manifest.235e324f21acc8bdc1e33604cab9ffb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c24d400ad4fc5e8ef9c",
+    "url": "/static/css/main.1652ab92.chunk.css"
+  },
+  {
+    "revision": "6c24d400ad4fc5e8ef9c",
+    "url": "/static/js/main.b1eb9d75.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07122a623c4e73f06132",
+    "url": "/static/css/2.efa316bf.chunk.css"
+  },
+  {
+    "revision": "07122a623c4e73f06132",
+    "url": "/static/js/2.fbd2701d.chunk.js"
+  },
+  {
+    "revision": "6a3ea6bae5471f990a13e15cf163b0a8",
+    "url": "/index.html"
+  }
+];
